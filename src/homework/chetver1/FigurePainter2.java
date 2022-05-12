8ac987bc89a33d3b7480178021cd66f907dffa18
Davit Voskanyan <davvoskanyan9507@gmail.com>
2022-05-12 12:04:14 +0400

    FigurePainter2

---
+++ src/homework/chetver1/FigurePainter2.java
@@ -0,0 +1,40 @@
+package homework.chetver1;
+
+public class FigurePainter2 {
+    public static void main(String[] args) {
+
+      /*  for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+
+       for(int l=0;l< 5;l++){
+        for(int m=5;m>l;m--){
+        System.out.print(" ");
+        }
+        for(int m=0;m<l; m++){
+        System.out.print("* ");
+        }
+        System.out.println();
+       }
+        for(int l=0;l<5;l++){
+        for(int m=0;m<l; m++){
+        System.out.print(" ");
+        }
+        for(int m=5;m>l;m--){
+        System.out.print("* ");
+        }
+        System.out.println();
+
+        }
+       }
+        }
