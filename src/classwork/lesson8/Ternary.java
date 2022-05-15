49fa5d4e607f76fb1a64f6bf820f7eecd542c52a
Davit Voskanyan <davvoskanyan9507@gmail.com>
2022-05-15 21:22:31 +0400

    Ternary

---
+++ src/classwork/lesson8/Ternary.java
@@ -0,0 +1,16 @@
+package classwork.lesson8;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i = 10;
+        int k = i < 0 ? -i : i;
+        System.out.print("Абсолиютное значение");
+        System.out.println(i + "равно" + k);
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсалютное значение");
+        System.out.print("Абсалютное значение");
+        System.out.println(i + "равно" + k);
+
+    }
+}
