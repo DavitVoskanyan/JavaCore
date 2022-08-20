a2f8cef4275437065a53e617a150ffb8307029fe
Davit Voskanyan <davvoskanyan9507@gmail.com>
2022-08-20 17:43:01 +0400

    JDBCExample

---
+++ src/main/java/jdbcExample/Main.java
@@ -0,0 +1,35 @@
+package jdbcExample;
+
+import jdbcExample.manager.UserManager;
+import jdbcExample.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        try {
+           // User user=new  User("pxos", "poxosyan", "poxos@poxos.com", "poxos");
+          //  userManager.addUser (user);
+            //System.out.println(user);
+            List<User> allUsers=userManager.getAllUsers();
+            for (User user : allUsers) {
+                System.out.println(user);
+            }
+            userManager.deleteUserById(4);
+
+            allUsers=userManager.getAllUsers();
+
+            for (User user : allUsers) {
+                System.out.println(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/main/java/jdbcExample/SingletonExample.java
@@ -0,0 +1,11 @@
+package jdbcExample;
+
+public class SingletonExample {
+    private  static SingletonExample instance  =new SingletonExample();
+    private SingletonExample(){
+
+    }
+    public static SingletonExample getInstance(){
+        return  instance;
+    }
+}
